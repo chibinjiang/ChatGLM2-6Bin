12c2662fa0a3b765fb3a8f1a4d52fe0ef9f7f837
chibinjiang <jiangzhibin@yolonet.io>
2023-09-13 01:26:23 +0800

    add scripts

---
+++ scripts/test_checkpoint.py
@@ -0,0 +1,38 @@
+from transformers import AutoConfig, AutoModel, AutoTokenizer
+
+
+def main():
+    model_path = '../models'
+    tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+    config = AutoConfig.from_pretrained(model_path, trust_remote_code=True, pre_seq_len=128)  # pre_seq_len 同训练用的
+    model = AutoModel.from_pretrained(model_path, config=config, trust_remote_code=True)
+    # load checkpoints
+    CHECKPOINT_PATH = 'ptuning/output/adgen-chatglm2-6b-pt-128-2e-2/checkpoint-1000'
+    prefix_state_dict = torch.load(os.path.join(CHECKPOINT_PATH, "pytorch_model.bin"))
+    new_prefix_state_dict = {}
+    for k, v in prefix_state_dict.items():
+        if k.startswith("transformer.prefix_encoder."):
+            new_prefix_state_dict[k[len("transformer.prefix_encoder."):]] = v
+
+    # 量化 4 bit
+    model.transformer.prefix_encoder.load_state_dict(new_prefix_state_dict)
+    model = model.half().cuda()
+    model.transformer.prefix_encoder.float()
+    model = model.eval()
+    # generate text
+
+    def ask(question, history=[]):
+        response, new_history = model.chat(tokenizer, question, history=history)
+        print(f">>> 问: {question}")
+        print(f">>> 答: {response}")
+        return new_history
+
+    context = ask("你好")
+    context = ask("我不喜欢这款休闲裤", context)
+    context = ask("咱们能不能不聊裤子", context)
+    context = ask("进程和线程有什么区别", context)
+    context = ask("Python的深拷贝和浅拷贝有什么区别", context)
+
+
+if __name__ == '__main__':
+    main()
